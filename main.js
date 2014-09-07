e05b155b23544209926ae54673b47b38d4fc342f
TJ Horner <rowenrocks@gmail.com>
2014-09-07 16:28:51 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+(function(){
+	var widget = SC.Widget('soundcloud-widget');
+	widget.bind(SC.Widget.Events.READY, function(){
+		var updatePercent = function(){
+			widget.getDuration(function(d1){
+				widget.getPosition(function(d2){
+					var percent = (d2 / d1) * 100;
+					$('.soundcloud-bar').css('width', percent + '%');
+				});
+			});
+		}
+
+		widget.setVolume(50);
+		$('.soundcloud').removeClass('loading').addClass('paused');
+		widget.getCurrentSound(function(s){
+			$('.soundcloud-art').css('background-image', 'url('+s.artwork_url+')');
+			$('.soundcloud-title').text(s.title);
+		});
+		
+		widget.bind(SC.Widget.Events.PLAY, function(s){
+			widget.getCurrentSound(function(s){
+				$('.soundcloud-art').css('background-image', 'url('+s.artwork_url+')');
+				$('.soundcloud-title').text(s.title);
+			});
+			$('.soundcloud').addClass('playing').removeClass('paused');
+		});
+		
+		widget.bind(SC.Widget.Events.FINISH, function(){
+			$('.soundcloud-bar').css('width', '0%');
+		})
+		
+		widget.bind(SC.Widget.Events.PAUSE, function(s){
+			$('.soundcloud').addClass('paused').removeClass('playing');
+		});
+		
+		widget.bind(SC.Widget.Events.PLAY_PROGRESS, function(s){
+			updatePercent();
+		});
+		
+		$('.soundcloud').click(function(){
+			widget.toggle();
+		});
+
+		$('.soundcloud-button.forward').click(function(){
+			widget.next();
+			updatePercent();
+		});
+
+		$('.soundcloud-button.back').click(function(){
+			widget.prev();
+			updatePercent();
+		});
+	});
+}());
